f31eccc857795872851bb0f0e3e5cf69e3db4ee3
id774 <idnanashi@gmail.com>
2013-07-26 16:27:05 +0900

    News feed api from mongo example

--- app.rb
+++ app.rb
@@ -9,26 +9,34 @@ require 'mongo'
 class SinatraApiProvider < Sinatra::Base
   require './helpers/render_partial'
 
-  get '/' do
-    haml :index
-  end
-
-  get '/api.json' do
-    content_type :json, :charset => 'utf-8'
-
+  def initialize(app = nil, params = {})
+    super(app)
     @run_date      = Date.today
-    @pickup_date   = (@run_date - 1).strftime("%Y%m%d")
-    @today         = @run_date.strftime("%Y%m%d")
-
     @mongo         = Mongo::Connection.new('localhost', 27017)
     @db            = @mongo.db('fluentd')
-    @coll          = @db.collection('raspberry.temperature')
+    @coll          = @db.collection('news.feed')
+  end
 
-    from = Time.parse(@pickup_date)
-    to   = Time.parse(@today)
+  #get '/' do
+  #  haml :index
+  #end
+
+  get '/news.today' do
+    content_type :json, :charset => 'utf-8'
+
+    from = Time.parse((@run_date).strftime("%Y%m%d"))
+    @json = @coll.find({:time => {"$gt" => from}})
+    @json.to_a.to_json
+  end
+
+  get '/news.yesterday' do
+    content_type :json, :charset => 'utf-8'
+
+    from = Time.parse((@run_date - 1).strftime("%Y%m%d"))
+    to   = Time.parse((@run_date).strftime("%Y%m%d"))
     @json = @coll.find({:time => {"$gt" => from , "$lt" => to}})
     @json.to_a.to_json
- end
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
