575253cf469c77b40f7c4a8d0a5f922f1b866fba
id774 <idnanashi@gmail.com>
2013-07-26 15:58:55 +0900

    Daily Temperature provide example

--- app.rb
+++ app.rb
@@ -15,9 +15,20 @@ class SinatraApiProvider < Sinatra::Base
 
   get '/api.json' do
     content_type :json, :charset => 'utf-8'
-    data = { "ほげ" => 1, "ふが" => 2 }
-    topics = JSON.generate(data)
-  end
+
+    @run_date      = Date.today
+    @pickup_date   = (@run_date - 1).strftime("%Y%m%d")
+    @today         = @run_date.strftime("%Y%m%d")
+
+    @mongo         = Mongo::Connection.new('localhost', 27017)
+    @db            = @mongo.db('fluentd')
+    @coll          = @db.collection('raspberry.temperature')
+
+    from = Time.parse(@pickup_date)
+    to   = Time.parse(@today)
+    @json = @coll.find({:time => {"$gt" => from , "$lt" => to}})
+    @json.to_a.to_json
+ end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- tasks/application.rake
+++ tasks/application.rake
@@ -1,4 +1,4 @@
 desc 'Run the app'
 task :s do
-  system "rackup -p 4567"
+  system "rackup -p 3000 -o 0.0.0.0"
 end
