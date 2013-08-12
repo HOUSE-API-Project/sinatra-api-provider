543ce1b8be8ef23bf2a90683ca91ff71d100bcdf
id774 <idnanashi@gmail.com>
2013-08-12 10:36:04 +0900

    Implement for one url

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require 'haml'
 require 'json'
+require 'date'
 require 'time'
 require 'mongo'
 
@@ -13,8 +14,33 @@ class SinatraApiProvider < Sinatra::Base
   def initialize(app = nil, params = {})
     super(app)
     @mongo    = Mongo::Connection.new('localhost', 27017)
-    @db       = @mongo.db('fluentd')
-    @coll     = @db.collection('news.feed')
+    @db       = @mongo.db('houseapi')
+  end
+
+  def time_parser
+    params   = Rack::Utils.parse_query(@env['rack.request.query_string'])
+
+    if params['from']
+      begin
+        from = Time.strptime(params['from'], "%Y%m%d%H%M%S")
+      rescue ArgumentError
+        from = Time.parse((Date.today - 1).strftime("%Y%m%d"))
+      end
+    else
+      from = Time.parse((Date.today - 1).strftime("%Y%m%d"))
+    end
+
+    if params['to']
+      begin
+        to = Time.strptime(params['to'], "%Y%m%d%H%M%S")
+      rescue ArgumentError
+        to = Time.parse((Date.today + 1).strftime("%Y%m%d"))
+      end
+    else
+      to = Time.parse((Date.today + 1).strftime("%Y%m%d"))
+    end
+
+    return from, to
   end
 
   configure :development, :production do
@@ -25,30 +51,16 @@ class SinatraApiProvider < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get '/news' do
-    content_type :json, :charset => 'utf-8'
-    @params   = Rack::Utils.parse_query(@env['rack.request.query_string'])
-    if @params['from']
-      begin
-        from = Time.strptime(@params['from'], "%Y%m%d%H%M%S")
-      rescue ArgumentError
-        from = Time.today
-      end
-    else
-      from = Time.today
-    end
-    if @params['to']
-      begin
-        to = Time.strptime(@params['to'], "%Y%m%d%H%M%S")
-      rescue ArgumentError
-        to = Time.today
-      end
-    else
-      to = Time.today
-    end
+  get '/' do
+    haml :index
+  end
 
-    @json = @coll.find({:time => {"$gt" => from , "$lt" => to}})
-    @json.to_a.to_json
+  get '/shibuhouse.bf_kunugi_sound' do
+    content_type :json, :charset => 'utf-8'
+
+    @coll = @db.collection('shibuhouse.bf_kunugi_sound')
+    from, to = time_parser
+    @json = @coll.find({:time => {"$gt" => from , "$lt" => to}}).to_a.to_json
   end
 
   # start the server if ruby file executed directly
